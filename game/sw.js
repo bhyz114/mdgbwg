8aa73920eb77dc054b3c8ac62afe5a9ffecec221
胞核因子 <2101683339@qq.com@example.com>
2026-08-26 13:03:45 +0800

    Deploy latest game build and cache update

--- game/sw.js
+++ game/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "caitao-oath-exhibit-";
-const CACHE_NAME = `${CACHE_PREFIX}v28`;
+const CACHE_NAME = `${CACHE_PREFIX}v31`;
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./pwa-icon.svg"];
 
 self.addEventListener("install", event => {
@@ -49,3 +49,6 @@ self.addEventListener("fetch", event => {
       }))
   );
 });
+
+
+
