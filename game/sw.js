405096f9bbcbba5b8400344ff6eca8aa59c889b1
胞核因子 <2101683339@qq.com@example.com>
2026-08-26 15:58:14 +0800

    Add save overwrite confirmation

--- game/sw.js
+++ game/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "caitao-oath-exhibit-";
-const CACHE_NAME = `${CACHE_PREFIX}v31`;
+const CACHE_NAME = `${CACHE_PREFIX}v32`;
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./pwa-icon.svg"];
 
 self.addEventListener("install", event => {
